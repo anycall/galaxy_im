88c3d4af834496f738d3b7f10544db15c6999904
garfieldbear333 <127082432+garfieldbear333@users.noreply.github.com>
2023-12-20 15:16:01 +0800

    添加用户页

---
+++ lib/Pages/Contacts/AddUser.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddUserPage extends StatefulWidget {
+  const AddUserPage({super.key});
+
+  @override
+  State<AddUserPage> createState() => _AddUserPageState();
+}
+
+class _AddUserPageState extends State<AddUserPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
